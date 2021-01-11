7d846ac8fdb5ab719a58dcf5a244951e4f86589e
madelinemc <madeline.coons@gmail.com>
2021-01-10 20:59:14 -0600

    commit tree b94cf92de55105979a12bea212c467a184eb536f

---
+++ index.js
@@ -0,0 +1,35 @@
+function wakeDog(dogName, dogBreed) {
+    return (`Wake ${dogName} the ${dogBreed}`);
+}
+
+function leashDog(dogName, dogBreed) {
+    return (`Leash ${dogName} the ${dogBreed}`);
+}
+
+function walkToPark(dogName, dogBreed) {
+    return (`Walk to the park with ${dogName} the ${dogBreed}`); 
+}
+
+function throwFrisbee(dogName, dogBreed) {
+    return (`Throw the frisbee for ${dogName} the ${dogBreed}`);
+}
+
+function walkHome(dogName, dogBreed) {
+    return (`Walk home with ${dogName} the ${dogBreed}`);
+}
+
+function unleashDog(dogName, dogBreed) {
+    return (`Unleash ${dogName} the ${dogBreed}`);
+}
+
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+
+function exerciseDog(dogName, dogBreed) {
+    let i = 0;
+    let dogArray = [];
+    while (i < routine.length) {
+        dogArray.push(routine[i](dogName, dogBreed));
+        i++; 
+    }
+    return dogArray;
+}
